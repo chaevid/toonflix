209330400e18e0071d691f74e9bc7d37fa16ae6f
chaevid <chaevid@gmail.com>
2023-05-09 19:31:34 +0900

    Apply Material Design 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const seedColor = Color(0xFFA83DFF);
 
-  // This widget is the root of your application.
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: seedColor,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: seedColor,
+        brightness: Brightness.dark,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Hello World'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Text("Hello"),
         ),
       ),
     );
   }
 }
+
+// class App extends StatelessWidget {
+//   const App({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//           useMaterial3: true,
+//           colorScheme: ColorScheme.fromSwatch(
+//             primarySwatch: Colors.purple,
+//           )
+//           // colorScheme: ColorScheme(``)
+//           ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Hello World'),
+//         ),
+//         body: const Center(
+//           child: Text('Hello World'),
+//         ),
+//       ),
+//     );
+//   }
+// }
