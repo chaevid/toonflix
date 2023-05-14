14a84ab8704ae8ce16094341e821dceba0433cbe
chaevid <chaevid@gmail.com>
2023-05-15 03:11:22 +0900

    Flutter Challenge FINAL - UI - Detail

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import '../api.dart';
@@ -7,7 +9,7 @@ class DetailScreen extends StatelessWidget {
   final int id;
   final api = MovieApi();
 
-  DetailScreen({super.key, required this.id});
+  DetailScreen({Key? key, required this.id}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,55 +23,116 @@ class DetailScreen extends StatelessWidget {
         } else {
           Movie movie = snapshot.data!;
           return Scaffold(
+            extendBodyBehindAppBar: true,
             appBar: AppBar(
-              title: Text(movie.title),
+              title: const Text('Back to list'),
+              titleTextStyle:
+                  const TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: false,
             ),
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.network(
-                      'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                      width: 200,
-                      height: 300,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      movie.title,
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
+            body: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.network(
+                    'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const Positioned.fill(
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.transparent, Colors.black],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.2, 0.95],
                       ),
                     ),
-                    SizedBox(height: 16.0),
-                    Row(
-                      children: List.generate(
-                        5,
-                        (index) => Icon(
-                          Icons.star,
-                          color: index < movie.voteAverage ~/ 2
-                              ? Colors.yellow
-                              : Colors.grey[300],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 120, 16, 140),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        movie.title,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(movie.overview),
-                    SizedBox(height: 16.0),
-                    Text(
-                      'Genres: ' +
-                          movie.genres.map((genre) => genre.name).join(', '),
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontStyle: FontStyle.italic,
+                      const SizedBox(height: 4),
+                      Row(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star_rounded,
+                            color: index < movie.voteAverage ~/ 2
+                                ? Colors.yellow
+                                : Colors.white54,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      Wrap(
+                        children: [
+                          const Text(
+                            '2h 14min | ',
+                            style:
+                                TextStyle(fontSize: 12, color: Colors.white70),
+                          ),
+                          Text(
+                            movie.genres.map((genre) => genre.name).join(', '),
+                            style: const TextStyle(
+                                fontSize: 12, color: Colors.white70),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 24),
+                      const Text('Storyline',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                      const SizedBox(height: 8),
+                      Text(movie.overview,
+                          style: const TextStyle(
+                              fontSize: 14, color: Colors.white)),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 12,
+                  left: 24,
+                  right: 24,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(16),
+                        backgroundColor: Colors.yellow[600],
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        log('Button : Buy Ticket');
+                      },
+                      child: const Text(
+                        'Buy Ticket',
+                        style: TextStyle(fontWeight: FontWeight.w700),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           );
         }
